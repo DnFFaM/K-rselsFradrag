196ca017eb6de66181a012d80c7f3def53be415c
ATec <111857421+DnFFaM@users.noreply.github.com>
2023-06-23 13:53:40 +0200

    + Unit test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Technical Education Copenhagen")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Technical Education Copenhagen 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d984e4e-00a0-420e-bfef-38f650c97a3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using KørselsFradrag;
+
+namespace KørselsFradragTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void UdenBro()
+        {
+            double kilometer = 150;
+            double dage = 1;
+            int choice = 0;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(239,76, result);
+
+        }
+        [TestMethod]
+        public void UdenBroM10Dage()
+        {
+            double kilometer = 150;
+            double dage = 10;
+            int choice = 0;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(239, 76, result);
+
+        }
+        [TestMethod]
+        public void MedBro1()
+        {
+            double kilometer = 150;
+            double dage = 1;
+            int choice = 1;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(259,76, result);
+
+        }
+        [TestMethod]
+        public void MedBro1M10Dage()
+        {
+            double kilometer = 150;
+            double dage = 10;
+            int choice = 2;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(239, 76, result);
+
+        }
+        [TestMethod]
+        public void MedBro2()
+        {
+            double kilometer = 150;
+            double dage = 1;
+            int choice = 2;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(239, 76, result);
+
+        }
+        [TestMethod]
+        public void MedBro2M10Dage()
+        {
+            double kilometer = 150;
+            double dage = 10;
+            int choice = 2;
+            Beregn beregn = new Beregn(kilometer, dage, choice);
+
+            // Act
+            double result = beregn.KF();
+
+            // Assert
+            Assert.AreEqual(239, 76, result);
+
+        }
+    }
+}
